c0ca50684a32c8cf58074a4adef1875aa1e70783
Jakob Ruhe <jakob.ruhe@gmail.com>
2020-05-10 21:15:49 +0200

    Use symbolic links instead of copying files
    This makes it possible to actualy keep things up to date.

--- fetch.sh
+++ fetch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mkdir -p downloads/emacs
-wget https://raw.githubusercontent.com/hrs/sensible-defaults.el/master/sensible-defaults.el --output-document downloads/emacs/sensible-defaults.el
+wget https://raw.githubusercontent.com/hrs/sensible-defaults.el/master/sensible-defaults.el --output-document emacs/sensible-defaults.el
--- setup.sh
+++ setup.sh
@@ -1,6 +1,29 @@
 #!/bin/sh
+
+set -e
+
+# From:
+# https://stackoverflow.com/questions/1055671/how-can-i-get-the-behavior-of-gnus-readlink-f-on-a-mac
+readlinkf(){ perl -MCwd -e 'print Cwd::abs_path shift' "$1";}
+
+SCRIPT="$(readlinkf $0)"
+DIR="$(dirname $SCRIPT)"
+
 echo Setting up Emacs...
 mkdir -p $HOME/.emacs.d
-cp emacs/configuration.org $HOME/.emacs.d/.
-cp emacs/init.el $HOME/.emacs.d/.
-cp downloads/emacs/sensible-defaults.el $HOME/.emacs.d/.
+
+EMACS_FILES=("configuration.org" "init.el" "sensible-defaults.el")
+
+for f in ${EMACS_FILES[@]};
+do
+    TO=$DIR/emacs/$f
+    FROM=$HOME/.emacs.d/$f
+    if [ "$(readlinkf $FROM)" = "$TO" ]; then
+	echo Symlink $FROM already pointing to $TO
+    else
+	Echo Creating symlink "$FROM" to "$TO"...
+	ln -s "$TO" "$FROM"
+    fi
+done
+
+echo Operation completed.
