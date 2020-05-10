b07831fde5d4d6fb68c79cd15d61fdd0b21feeba
Jakob Ruhe <jakob.ruhe@gmail.com>
2020-05-10 22:45:23 +0200

    Setup things using functions and use symlinks for git and inputrc

--- setup.sh
+++ setup.sh
@@ -1,34 +1,73 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
+# Get absolute path of a file.
 # From:
 # https://stackoverflow.com/questions/1055671/how-can-i-get-the-behavior-of-gnus-readlink-f-on-a-mac
-readlinkf(){ perl -MCwd -e 'print Cwd::abs_path shift' "$1";}
+readlinkf() {
+    perl -MCwd -e 'print Cwd::abs_path shift' "$1"
+}
+
+# Create a symlink.
+function symlink() {
+    declare TARGET=$1
+    declare LINK_NAME=$2
+    if [ "$(readlinkf $LINK_NAME)" = "$TARGET" ]; then
+	    echo Symlink $LINK_NAME already points to $TARGET
+    else
+	    echo Creating symlink $LINK_NAME to $TARGET...
+	    ln --symbolic "$TARGET" "$LINK_NAME"
+    fi
+}
+
+# Download a file if it does not exist already.
+function download() {
+    declare SOURCE=$1
+    declare TARGET=$2
+    if [[ ! -f "$TARGET" ]]; then
+        echo Downloading $SOURCE to $TARGET...
+        mkdir -p "$(dirname "$TARGET")"
+        wget "$SOURCE" --output-document "$TARGET"
+    fi
+}
+
+function makedir() {
+    declare DIR=$1
+    if [[ ! -d "$DIR" ]]; then
+        echo Creating directory $DIR...
+        mkdir -p "$DIR"
+    fi
+}
+
+function setup_emacs() {
+    echo Setting up Emacs...
+    makedir "$HOME/.emacs.d"
+    download \
+        "https://raw.githubusercontent.com/hrs/sensible-defaults.el/master/sensible-defaults.el" \
+        "downloads/emacs/sensible-defaults.el"
+    symlink "$SCRIPT_DIR/emacs/init.el" "$HOME/.emacs.d/init.el"
+    symlink "$SCRIPT_DIR/emacs/configuration.org" "$HOME/.emacs.d/configuration.org"
+    symlink "$SCRIPT_DIR/downloads/emacs/sensible-defaults.el" "$HOME/.emacs.d/sensible-defaults.el"
+}
+
+function setup_inputrc() {
+    echo Setting up inputrc...
+    symlink "$SCRIPT_DIR/inputrc/dot.inputrc" "$HOME/.inputrc"
+}
+
+setup_git() {
+    echo Setting up git...
+    symlink "$SCRIPT_DIR/git/dot.gitconfig" "$HOME/.gitconfig"
+}
+
+# Main
 
 SCRIPT="$(readlinkf $0)"
-DIR="$(dirname $SCRIPT)"
+SCRIPT_DIR="$(dirname $SCRIPT)"
 
-echo Setting up Emacs...
-mkdir -p $HOME/.emacs.d
-
-EMACS_FILES=("configuration.org" "init.el" "sensible-defaults.el")
-
-for f in ${EMACS_FILES[@]};
-do
-    TO=$DIR/emacs/$f
-    FROM=$HOME/.emacs.d/$f
-    if [ "$(readlinkf $FROM)" = "$TO" ]; then
-	echo Symlink $FROM already pointing to $TO
-    else
-	Echo Creating symlink "$FROM" to "$TO"...
-	ln -s "$TO" "$FROM"
-    fi
-done
+setup_emacs
+setup_inputrc
+setup_git
 
 echo Operation completed.
-
-echo Setting up inputrc...
-cp inputrc/dot.inputrc $HOME/.inputrc
-echo Setting up git...
-cp git/dot.gitconfig $HOME/.gitconfig
