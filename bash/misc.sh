a8df919ee02cf792589a455ad20e515ea0f55fb8
Jakob Ruhe <jakob.ruhe@gmail.com>
2020-08-11 22:52:58 +0200

    emacs: start emacs daemon if not running, run in current tty

--- bash/misc.sh
+++ bash/misc.sh
@@ -5,4 +5,4 @@ export PATH=/home/jakob/.local/bin:$PATH
 export PS1='\[\033[0;32m\]\[\033[0m\033[0;32m\]\u\[\033[0;36m\] @ \[\033[0;36m\]\h \w\[\033[0;32m\]$(__git_ps1)\n\[\033[0;32m\]└─\[\033[0m\033[0;32m\] \$\[\033[0m\033[0;32m\] ▶\[\033[0m\] '
 
 # Emacs client
-alias emacs=emacsclient
+alias emacs="emacsclient --tty --alternate-editor="
