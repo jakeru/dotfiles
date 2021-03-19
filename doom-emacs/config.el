ec4982790c172b03d88734d848e56caa976ec6b0
Jakob Ruhe <jakob.ruhe@gmail.com>
2021-03-19 22:25:50 +0100

    doom-emacs: org-roam and theme change

--- doom-emacs/config.el
+++ doom-emacs/config.el
@@ -24,11 +24,15 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-acario-dark)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/Notes/org")
+
+;; org-roam configuration
+(setq org-roam-directory "~/Notes/org/roam")
+(add-hook 'after-init-hook 'org-roam-mode)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
