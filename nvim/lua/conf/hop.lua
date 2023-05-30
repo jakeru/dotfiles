eded261d038e94f4fab88de6a2d1a075701f5d04
Jakob Ruhe <jakob.ruhe@gmail.com>
2023-05-30 21:32:49 +0200

    nvim/hop: Hop also in visual mode

--- nvim/lua/conf/hop.lua
+++ nvim/lua/conf/hop.lua
@@ -11,19 +11,25 @@ hop.setup({})
 local jlib = require('jlib')
 local directions = require('hop.hint').HintDirection
 
-jlib.nmap('s', function()
+local modes = {'n', 'v', 'x'}
+
+-- Jump forward on the current line.
+jlib.map(modes, 's', function()
   hop.hint_char2({ direction = directions.AFTER_CURSOR, current_line_only = true })
 end, 'Hop 2 char same line')
 
-jlib.nmap('<leader>j', function()
+-- Jump to anywhere on the screen.
+jlib.map(modes, '<leader>j', function()
   hop.hint_char2()
 end, 'Hop 2 char')
 
-jlib.nmap('<leader>w', function()
+-- Jump to a word on the screen.
+jlib.map(modes, '<leader>w', function()
   hop.hint_words()
 end, 'Hop word')
 
-jlib.nmap('<leader>l', function()
+-- Jump to line on the screen.
+jlib.map(modes, '<leader>l', function()
   hop.hint_lines_skip_whitespace()
 end, 'Hop line')
 
