82f40355f9dc76ff893fd448329ccdc43d564687
Jakob Ruhe <jakob.ruhe@gmail.com>
2023-09-03 23:26:25 +0200

    nvim: Add plugin nvim_lsp_signature_help
    This shows function parameters when calling a function.

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -154,6 +154,7 @@ return {
         'hrsh7th/nvim-cmp',
         dependencies = {
             'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-nvim-lsp-signature-help',
             'hrsh7th/cmp-buffer',
             'hrsh7th/cmp-path',
             'saadparwaiz1/cmp_luasnip',
