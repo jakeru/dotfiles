b256272e0b82af1900c5b50c29dd13ee18f4dd21
Jakob Ruhe <jakob.ruhe@gmail.com>
2023-09-03 23:32:13 +0200

    nvim: Install language servers pyright and clangd

--- nvim/lua/conf/lsp.lua
+++ nvim/lua/conf/lsp.lua
@@ -72,12 +72,11 @@ end
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
-    -- clangd = {},
     -- gopls = {},
-    -- pyright = {},
     -- rust_analyzer = {},
     -- tsserver = {},
-
+    clangd = {},
+    pyright = {},
     lua_ls = {
         Lua = {
             workspace = { checkThirdParty = false },
