e71ab23478a14fd24ab0de3d3cba5b895096b07a
Jakob Ruhe <jakob.ruhe@gmail.com>
2025-07-02 22:49:57 +0200

    nvim: Add luasnip snippet for C as a reference

---
+++ nvim/lua/snippets/c.lua
@@ -0,0 +1,17 @@
+local ls = require("luasnip")
+
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+
+return {
+    s("stru", {
+        t({"typedef struct {"}),
+        t({"", "\t"}),
+        i(2, ""),
+        t({"", "} "}),
+        i(1, ""),
+        t({";"}),
+    }),
+}
